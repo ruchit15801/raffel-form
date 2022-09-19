3ff206980559abbc874fa6c1d9561ce8e95b9bc5
rashmi <rashmi.pullur@technomads.in>
2022-09-19 15:37:46 +0530

    changes by rashmi in raffel

--- src/controller/nft.js
+++ src/controller/nft.js
@@ -1,4 +1,6 @@
 const Raffel = require("../model/raffel");
+const WalletUser = require("../model/usermodel");
+const HTTP = require("../constant/response.code")
 
 const newRaffel = async (req, res) => {
   try {
@@ -20,9 +22,93 @@ const newRaffel = async (req, res) => {
 
     return res.status(200).send({ data: user });
   } catch (e) {
-    console.log(e);
-    res.status(500).json(e);
+    console.log(err);
+    return res.status(HTTP.SUCCESS).send({
+        status: false,
+        code: HTTP.INTERNAL_SERVER_ERROR,
+        message: 'Something went wrong!',
+        data: {},
+    });
   }
 };
 
-module.exports = { newRaffel };
+const addWallet = async (req, res) => {
+  try {
+    const { wallet,collection_name,nft_name ,token_address} = req.body;
+        if (!req.body || !wallet || !collection_name || !nft_name || !token_address)
+            return res.status(HTTP.SUCCESS).send({
+                status: false,
+                code: HTTP.NOT_FOUND,
+                message: 'All fields are required!',
+                data: {},
+            });
+  
+
+        // const walletExists = await WalletUser.findOne({
+        //     wallet
+        // });
+        // if (walletExists)
+        //     return res.status(HTTP.SUCCESS).send({
+        //         success: false,
+        //         code: HTTP.BAD_REQUEST,
+        //         message: 'Wallet already exists with same name!',
+        //         data: {},
+        //     });
+        const walletData = await new WalletUser({
+            wallet,collection_name,nft_name ,token_address
+        }).save();
+        if (!walletData)
+            return res.status(HTTP.SUCCESS).send({
+                status: false,
+                code: HTTP.BAD_REQUEST,
+                message: 'Unable to save wallet data',
+                data: {},
+            });
+        return res.status(HTTP.SUCCESS).send({
+            status: true,
+            code: HTTP.SUCCESS,
+            message: 'wallet data.',
+            data: {walletData},
+        });
+  } catch(err) {
+    console.log(err);
+    return res.status(HTTP.SUCCESS).send({
+        status: false,
+        code: HTTP.INTERNAL_SERVER_ERROR,
+        message: 'Something went wrong!',
+        data: {},
+    });
+}
+}
+
+const getinfo = async (req, res) => {
+  try{
+
+    const wallet_data = await WalletUser.find({},{ _id: 0, __v: 0 })
+    const nftRaffel_data = await Raffel.find({},{ _id: 0, __v: 0})
+    const data = []
+    data.push({ wallet_data, nftRaffel_data })
+    return res.status(HTTP.SUCCESS).send({
+      status: true,
+      code: HTTP.SUCCESS,
+      message: 'sara data',
+      data: data ,
+  });
+
+  } catch(err) {
+    console.log(err);
+    return res.status(HTTP.SUCCESS).send({
+        status: false,
+        code: HTTP.INTERNAL_SERVER_ERROR,
+        message: 'Something went wrong!',
+        data: {},
+    });
+  }
+}
+
+module.exports = { 
+  newRaffel,
+  addWallet,
+  getinfo
+
+};
--- src/router/nftraffel.js
+++ src/router/nftraffel.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const raffelData = require("../controller/nft");
 
-router.post("/api/nft", raffelData.newRaffel);
+router.post('/api/nft', raffelData.newRaffel);
+router.post('/api/addWallet', raffelData.addWallet);
+router.get('/getinfo', raffelData.getinfo)
 
 module.exports = router;

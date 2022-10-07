7cba32a79cb32021102d4b7905f823b2582ff124
avi-technomads <avi.patel@technomads.in>
2022-10-07 15:33:24 +0530

    new change by yagnik and new get api make

--- src/router/nftraffel.js
+++ src/router/nftraffel.js
@@ -7,14 +7,14 @@ router.post("/createRaffel", raffel_controller.createRaffel);
 // router.get("/getinfo", raffel_controller.getinfo);
 
 // buy ticket
-router.post('/buyTicket', raffel_controller.buyTicket)
-router.post('/ticketRemaining', raffel_controller.ticketRemaining)
-router.post('/checkRemaining', raffel_controller.checkRemaining)
-router.post('/participants', raffel_controller.participants)
-router.get('/getAllData', raffel_controller.getAllData)
+router.post("/buyTicket", raffel_controller.buyTicket);
+router.post("/ticketRemaining", raffel_controller.ticketRemaining);
+router.post("/checkRemaining", raffel_controller.checkRemaining);
+router.post("/participants", raffel_controller.participants);
+router.get("/getAllData", raffel_controller.getAllData);
+router.post("/getsingleData", raffel_controller.getSingleData);
 
-router.post('/getWinner', raffel_controller.getWinner)
-
+router.post("/getWinner", raffel_controller.getWinner);
 
 // stop watch
 // router.post('/stopwatch', raffel_controller.stopwatch)
